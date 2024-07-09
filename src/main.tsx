650eefd22f25cd83d3b44752b1c25eaf1694e221
Adil Ahmad <adilahmad0025@gmail.com>
2024-07-09 19:31:57 +0530

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';    
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
