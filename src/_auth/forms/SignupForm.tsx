e2c7c1736f994b10e3b6f762b34e8c332b251474
Adil Ahmad <adilahmad0025@gmail.com>
2024-07-11 16:50:28 +0530

    commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,6 +16,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import Loader from "@/components/shared/Loader"
+import { createUserAccount } from "@/lib/appwrite/api"
 
 
 
@@ -35,10 +36,11 @@ const SignupForm = () => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
+
+
+    console.log(newUser)
   }
   return (
     <Form {...form}>
